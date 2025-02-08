ccd82d146333d855f43332fb23c08fa733eb5871
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-02-08 20:47:11 +0300

    Add files via upload

---
+++ ConsoleAppCpp01_Exercise03/ConsoleAppCpp01_Exercise03/ConsoleAppCpp01_Exercise03.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>  
+#include <cmath>    
+
+int main() 
+{
+    system("chcp 1251");
+    double perimeter;
+    std::cout << "Введите периметр равностороннего треугольника: \n";
+    std::cin >> perimeter;
+
+    double side = perimeter / 3.0;
+
+    double semiperimeter = perimeter / 2.0;
+
+    double area = sqrt(semiperimeter * (semiperimeter - side) * (semiperimeter - side) * (semiperimeter - side));
+
+    std::cout << "Сторона: " << side << std::endl;
+    std::cout << "Площадь: " << area << std::endl;
+
+    return 0;
+}
