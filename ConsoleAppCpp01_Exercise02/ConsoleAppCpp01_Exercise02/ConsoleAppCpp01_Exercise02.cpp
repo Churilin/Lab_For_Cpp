0005d071440274f37d93e8ec4b9f2852946e913d
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-02-08 20:46:37 +0300

    Add files via upload

---
+++ ConsoleAppCpp01_Exercise02/ConsoleAppCpp01_Exercise02/ConsoleAppCpp01_Exercise02.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//cout.precision(3);
+	//system("chcp 1251");
+	//double a, b;
+	//cout << "Enter a and b:\n";
+	//cin >> a;
+	//cin >> b;
+	//double x = a / b;
+	//cout << sizeof(a / b) << ends << sizeof(x) << endl;
+	//cout << "\n x = " << x << endl;
+	//return 0;
+	system("chcp 1251");
+	string name;
+	cout << "Введите свое имя\n";
+	double a, b;
+	cout << "Введите a и b:\n";
+	cin >> a;
+	cin >> name;
+	cin >> b;
+	double x = a / b;
+	cout << "\nx = " << x << endl;
+	cout << "Привет, " << name << "!\n";
+	return 0;
+}
