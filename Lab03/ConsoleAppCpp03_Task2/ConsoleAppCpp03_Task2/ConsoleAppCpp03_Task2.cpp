e99206cda43db17106a134a825dc466f070fe5b7
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-02-19 21:09:35 +0300

    Add files via upload

---
+++ Lab03/ConsoleAppCpp03_Task2/ConsoleAppCpp03_Task2/ConsoleAppCpp03_Task2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cmath>
+#include <stdexcept>
+#include <windows.h>
+
+using namespace std;
+
+double cubicRootPow(double a) {
+    if (a < 0) {
+        return -pow(-a, 1.0 / 3);
+    }
+    return pow(a, 1.0 / 3);
+}
+
+double cubicRootIterative(double a, double epsilon = 1e-6, int maxIterations = 1000) {
+    if (a == 0) return 0;
+
+    double x1 = a; 
+    double x2;
+
+    for (int i = 0; i < maxIterations; ++i) {
+        x2 = (1.0 / 3) * (a / (x1 * x1) + 2 * x1);
+
+        if (abs(x2 - x1) < epsilon) {
+            break;
+        }
+
+        x1 = x2;
+    }
+
+    return x2;
+}
+
+int main() {
+
+    SetConsoleOutputCP(1251);
+
+    double a;
+    cout << "Введите число для вычисления кубического корня: ";
+    cin >> a;
+
+    try {
+        double resultPow = cubicRootPow(a);
+        cout << "Кубический корень (pow): " << resultPow << endl;
+
+        double resultIterative = cubicRootIterative(a);
+        cout << "Кубический корень (итерационный): " << resultIterative << endl;
+    }
+    catch (const invalid_argument& e) {
+        cerr << "Ошибка: " << e.what() << endl;
+    }
+
+    return 0;
+}
