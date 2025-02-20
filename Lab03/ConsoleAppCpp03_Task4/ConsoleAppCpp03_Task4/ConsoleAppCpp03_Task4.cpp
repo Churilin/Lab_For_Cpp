e89d8894acee06749fe74dc79f632ef1718e8303
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-02-20 19:54:25 +0300

    Add files via upload

---
+++ Lab03/ConsoleAppCpp03_Task4/ConsoleAppCpp03_Task4/ConsoleAppCpp03_Task4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int sumSeries(int n) {
+    
+    if (n == 1) {
+        return 5;
+    }
+    
+    return 5 * n + sumSeries(n - 1);
+}
+
+int main() {
+
+    SetConsoleOutputCP(1251);
+
+    int n;
+    cout << "Введите значение n (n > 0): ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Ошибка: n должно быть больше 0.\n";
+    }
+    else {
+        int result = sumSeries(n);
+        cout << "Сумма ряда " << n << " = " << result << endl;
+    }
+
+    return 0;
+}
