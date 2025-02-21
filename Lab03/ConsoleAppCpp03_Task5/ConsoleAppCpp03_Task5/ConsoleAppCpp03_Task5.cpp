7ff2a385a3abb8806ac87f7f7f61bfea8c081627
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-02-21 17:16:52 +0300

    Add files via upload

---
+++ Lab03/ConsoleAppCpp03_Task5/ConsoleAppCpp03_Task5/ConsoleAppCpp03_Task5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void decimalToBinary(int num) {
+
+    if (num == 0) {
+        return;
+    }
+
+    decimalToBinary(num / 2);
+
+    cout << num % 2;
+}
+
+int main() {
+
+    SetConsoleOutputCP(1251);
+
+    int num;
+    cout << "Введите целое положительное число: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Ошибка: число должно быть положительным.\n";
+    }
+    else if (num == 0) {
+        cout << "Двоичное представление: 0\n";
+    }
+    else {
+        cout << "Двоичное представление: ";
+        decimalToBinary(num);
+        cout << endl;
+    }
+
+    return 0;
+}
+
