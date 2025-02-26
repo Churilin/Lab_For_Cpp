16d6fab9caacabfd3a18e636b78146ab8d7f3070
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-02-26 23:19:58 +0300

    Add files via upload

---
+++ Lab04/ConsoleAppCpp04_Task2/ConsoleAppCpp04_Task2/ConsoleAppCpp04_Task2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+bool Input(int& a, int& b) {
+    cout << "Введите два целых числа (a и b): ";
+    if (cin >> a >> b) {
+        return true; 
+    }
+    else {
+        cin.clear(); 
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+        return false; 
+    }
+}
+
+int main() {
+
+    system("chcp 1251");
+
+    int a, b;
+
+    if (!Input(a, b)) { 
+        cerr << "Ошибка ввода данных!" << endl;
+        return 1;
+    }
+
+    int s = a + b;
+    cout << "Сумма чисел: " << s << endl;
+
+    return 0;
+}
