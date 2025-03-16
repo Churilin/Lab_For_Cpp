a174be34a0f41d2c41e4810dde6cfb7905bcc676
Vladonchik <116621986+Churilin@users.noreply.github.com>
2025-03-16 21:48:16 +0300

    Add files via upload

---
+++ Lab14/ConsoleAppCpp14_Task2/ConsoleAppCpp14_Task2/ConsoleAppCpp14_Task2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+template <typename Container>
+void Print(const Container& container, const std::string& delimiter) {
+    bool first = true; 
+    for (const auto& element : container) {
+        if (!first) {
+            std::cout << delimiter; 
+        }
+        std::cout << element; 
+        first = false; 
+    }
+    std::cout << "\n"; 
+}
+
+int main() {
+    
+    std::vector<int> data = { 1, 2, 3 };
+    Print(data, ", "); 
+
+    return 0;
+}
